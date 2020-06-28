46402c234f4fcd95efb59ee137ae9cec05d2648c
Michael <michael.scully1997@gmail.com>
2020-06-28 02:14:46 -0400

    Scrapmap looks good

--- ScrapMap/urls.py
+++ ScrapMap/urls.py
@@ -19,14 +19,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from .utils import ObtainJSONWebToken
 
 from core import views
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('token-auth/', ObtainJSONWebToken.as_view()),
     path('core/', include('core.urls'))
 ]
 
